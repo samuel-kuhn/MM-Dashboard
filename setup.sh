63cb60c80a15e6a181a4bb509484993837012c0b
Samuel Kuhn <samuel.kuhn@gmx.ch>
2024-11-19 17:11:50 +0100

    use relative path for manage.py

--- setup.sh
+++ setup.sh
@@ -2,12 +2,12 @@
 SECRET=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 16)
 echo "SECRET_KEY=$SECRET" > .env
 
-python3 /app/manage.py makemigrations --no-input
+python3 manage.py makemigrations home --no-input
 
-python3 /app/manage.py migrate 
+python3 manage.py migrate 
 
-python3 /app/manage.py createsuperuser --no-input
+python3 manage.py createsuperuser --no-input
 
-python3 /app/manage.py collectstatic --noinput
+python3 manage.py collectstatic --noinput
 
 
