41bf06bf3f03f3e0ed1fea85f664ba791f59bf39
samuel <samuel.kuhn@gmx.ch>
2025-05-26 20:42:22 +0200

    structuring changes in api_functions.py

--- apps/home/api_functions.py
+++ apps/home/api_functions.py
@@ -11,33 +11,37 @@ def ping():
     except Exception:
         return False
 
+
 def get_servers(username):
     result = requests.get(f"{server_url}/containers", params={'username': username}).json()
     return result['servers']
+
 
 def start(username, server_name):
     response = requests.post(f"{server_url}/start", json={"username": username, "server_name": server_name})
     return response.status_code
 
+
 def stop(username, server_name):
     response = requests.post(f"{server_url}/stop", json={"username": username, "server_name": server_name})
     return response.status_code
+
 
 def reset(username, server_name):
     response = requests.post(f"{server_url}/reset", json={"username": username, "server_name": server_name})
     return response.status_code
 
+
 def delete(username, server_name):
     response = requests.post(f"{server_url}/delete", json={"username": username, "server_name": server_name})
     return response.status_code
 
+
 def create(username, data):
-    config_data = data
-    for key, value in config_data.items():
-        config_data[key] = value[0]
-    config_data = {"username": username, **config_data}
+    config_data = {"username": username, **data}
     response = requests.post(f"{server_url}/create", json=config_data)
     return response.status_code
+
 
 def edit(username, server_name, data):
     config_data = data
@@ -47,11 +51,13 @@ def edit(username, server_name, data):
     response = requests.post(f"{server_url}/edit", json=config_data)
     return response.status_code
 
+
 def get_server_config(username, server_name):
     result = requests.get(f"{server_url}/get-config", params={'username': username, 'server_name': server_name})
     display_config = result.json()
     display_config['MEMORY'] = display_config['MEMORY'][:1]
     return display_config
+
 
 def op(username, server_name, mc_user):
     data = {'username': username, 'server_name': server_name, 'mc_user': mc_user}
